947bc69088cb47a50648f8fcda4f8d80ef82de23
Carla Espino <carlaespino12@gmail.com>
2016-11-08 15:20:29 +0100

    -

--- countpresstime/countpresstime.ino
+++ countpresstime/countpresstime.ino
@@ -1,68 +1,49 @@
-int buttonPin = 7;  // the pin number for input (for me a push button)
-int ledPin = 13; 
-
-int current;         // Current state of the button
-                     // (LOW is pressed b/c i'm using the pullup resistors)
-long millis_held;    // How long the button was held (milliseconds)
-long prev_millis_held; // How long the button was held in the previous check
-byte previous = HIGH;
-unsigned long firstTime; // how long since the button was first pressed 
+int buttonPin = 13;  
+int led = 9;
+int brightness;
+int value;
+int buttonvalue;         
+                     
+long millis_held;    
+long last_millis; 
+byte last_buttonvalue = HIGH;
+unsigned long startpress;
 
 
 void setup() {
-  Serial.begin(9600);         // Use serial for debugging
-  pinMode(ledPin, OUTPUT);
-  digitalWrite(inPin, HIGH);  // Turn on 20k pullup resistors to simplify switch input
+  Serial.begin(9600);     
+  pinMode(buttonPin, INPUT_PULLUP);
 }
 
 void loop() {
-  current = digitalRead(inPin);
+  
+ last_buttonvalue = buttonvalue;
+ 
+    
+ buttonvalue = digitalRead(buttonPin);
 
-  // if the button state changes to pressed, remember the start time 
-  if (current == LOW && previous == HIGH && (millis() - firstTime) > 200) {
-    firstTime = millis();
+  if (buttonvalue == 0 && last_buttonvalue == 1) {
+    startpress = millis();
   }
 
-  millis_held = (millis() - firstTime);
+/*if (last_buttonvalue == 0 && buttonvalue==1){}*/
+    millis_held = (millis() - startpress);
 
-  // This if statement is a basic debouncing tool, the button must be pushed for at least
-  // 100 milliseconds in a row for it to be considered as a push.
-  if (millis_held > 50) {
 
-    if (current == LOW && secs_held > prev_secs_held) {
-      ledblink(1, 50, ledPin); // Each second the button is held blink the indicator led
-    }
 
-    // check if the button was released since we last checked
-    if (current == HIGH && previous == LOW) {
-      // HERE YOU WOULD ADD VARIOUS ACTIONS AND TIMES FOR YOUR OWN CODE
-      // ===============================================================================
-
-      // Button pressed for less than 1 second, one long LED blink
-      if (secs_held <= 0) {
-        ledblink(1,750,ledPin);
-      }
-
-      // If the button was held for 3-6 seconds blink LED 10 times
-      if (secs_held >= 1 && secs_held < 3) {
-        ledblink(10,200,ledPin); 
-      }
-
-      // Button held for 1-3 seconds, print out some info
-      if (secs_held >= 3) {
-        Serial.print("It Works!!! Seconds held: ");
-        Serial.print(secs_held);
-        Serial.print("   Milliseconds held: ");
-        Serial.println(millis_held);
-      }
-      // ===============================================================================
-    }
+ if(millis_held > 3000){
+   value=value+5;
+   
   }
+  if(millis_held<3000){
+    value=value-5;
+  }
+ Serial.println(value);
+analogWrite(led,value);
 
-  previous = current;
-  prev_secs_held = secs_held;
-}
 
+ 
 
+  
 }
 
---
+++ knightrider/I_NEED_HELP.ino
@@ -0,0 +1,64 @@
+int led1=11;
+int led2=10;
+int led3=9;
+int but1;
+int but2;
+int but3;
+int value1=255;
+int value2=255;
+int value3=255;
+
+void setup() {
+  // put your setup code here, to run once:
+pinMode(led1,OUTPUT);
+pinMode (led2, OUTPUT);
+pinMode (led3, OUTPUT);
+pinMode (but1, INPUT_PULLUP);
+pinMode (but2, INPUT_PULLUP);
+pinMode (but3, INPUT_PULLUP);
+Serial.begin(9600);
+}
+
+
+void loop() {
+  // put your main code here, to run repeatedly:
+ 
+ 
+if(millis()%1000==0){
+
+     value1=255;
+    value2=0;
+    value3=0;
+    delay(1);
+    analogWrite(led1,value1);
+  analogWrite(led2,value2);
+  analogWrite(led3,value3); 
+    
+  }if(millis()%2000==0){
+   
+            value1=0;
+      value2=255;
+      value3=0;
+      analogWrite(led1,value1);
+  analogWrite(led2,value2);
+  analogWrite(led3,value3); 
+delay(1);
+      
+        
+    }if(millis()%3000==0){
+    
+
+    
+      value1=0;
+      value2=0;
+          value3=255;
+          delay(1);
+          analogWrite(led1,value1);
+  analogWrite(led2,value2);
+  analogWrite(led3,value3); 
+    }
+    
+  
+}
+
+
